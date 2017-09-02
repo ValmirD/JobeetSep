0fba984fe84e5126b4f6d3076db6773fdb67ce9b
AMDFX2 <AMDFX2@AMDFX2-PC.lan>
2017-09-02 13:38:58 +0200

    formulaires

--- src/Ens/JobeetBundle/Form/JobType.php
+++ src/Ens/JobeetBundle/Form/JobType.php
@@ -9,33 +9,31 @@ use Ens\JobeetBundle\Entity\Job;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 
-class JobType extends AbstractType
-{
+class JobType extends AbstractType {
+
     /**
      * {@inheritdoc}
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-    	
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+
         $builder->add('categoryId')
-        		->add('type', ChoiceType::class, array('choices' => Job::getTypes(), 'expanded' => true))
-        		->add('company')
-        		->add('file', FileType::class, array('label' => 'Company logo', 'required' => false))
-		        ->add('url')
-		        ->add('position')
-		        ->add('location')
-		        ->add('description')
-		        ->add('howToApply', null, array('label' => 'How to apply?'))
-		        ->add('isPublic', null, array('label' => 'Public?'))
-		        ->add('isActivated')
-		        ->add('email');
+                ->add('type', ChoiceType::class, array('choices' => Job::getTypes(), 'expanded' => true))
+                ->add('company')
+                ->add('file', FileType::class, array('label' => 'Company logo', 'required' => false))
+                ->add('url')
+                ->add('position')
+                ->add('location')
+                ->add('description')
+                ->add('howToApply', null, array('label' => 'How to apply?'))
+                ->add('isPublic', null, array('label' => 'Public?'))
+                ->add('isActivated')
+                ->add('email');
     }
-    
+
     /**
      * {@inheritdoc}
      */
-    public function configureOptions(OptionsResolver $resolver)
-    {
+    public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'Ens\JobeetBundle\Entity\Job'
         ));
@@ -44,12 +42,8 @@ class JobType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function getBlockPrefix()
-    {
+    public function getBlockPrefix() {
         return 'ens_jobeetbundle_job';
     }
-    
-    
-
 
 }
