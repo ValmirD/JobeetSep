674b408c81f09fd9b6180d985b9d516188cd54c3
AMDFX2 <AMDFX2@AMDFX2-PC.lan>
2017-09-02 13:08:04 +0200

    phpunit

--- src/Ens/JobeetBundle/Tests/Utils/JobeetTest.php
+++ src/Ens/JobeetBundle/Tests/Utils/JobeetTest.php
@@ -1,14 +1,14 @@
 <?php
+
 namespace Ens\JobeetBundle\Tests\Utils;
+
 use Ens\JobeetBundle\Utils\Jobeet;
 
-class JobeetTest extends \PHPUnit_Framework_TestCase
-{
-	public function testSlugify()
-	
-	{
-		
-		mb_internal_encoding("UTF-8");
+class JobeetTest extends \PHPUnit_Framework_TestCase {
+
+    public function testSlugify() {
+
+        mb_internal_encoding("UTF-8");
 // 		$this->assertEquals('sensio', Jobeet::slugify('Sensio'));
 // 		$this->assertEquals('sensio-labs', Jobeet::slugify('sensio labs'));
 // 		$this->assertEquals('sensio-labs', Jobeet::slugify('sensio   labs'));
@@ -17,9 +17,9 @@ class JobeetTest extends \PHPUnit_Framework_TestCase
 // 		$this->assertEquals('sensio', Jobeet::slugify('sensio  '));
 // 		$this->assertEquals('n-a', Jobeet::slugify(''));
 // 		$this->assertEquals('n-a', Jobeet::slugify(' - '));
-		if (function_exists('iconv'))
-		{
-			$this->assertEquals('developpeur-web', Jobeet::slugify('Développeur Web'));
-		}
-	}
+        if (function_exists('iconv')) {
+            $this->assertEquals('developpeur-web', Jobeet::slugify('Dï¿½veloppeur Web'));
+        }
+    }
+
 }
