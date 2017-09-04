4c28d9f6a90da537495bff49551f4576c5a3833d
demaj <demaj@10.31.172.178>
2017-09-04 18:35:04 +0200

    generer l'entité

---
+++ src/Ens/JobeetBundle/Entity/Level.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Ens\JobeetBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Level
+ *
+ * @ORM\Table(name="level")
+ * @ORM\Entity(repositoryClass="Ens\JobeetBundle\Repository\LevelRepository")
+ */
+class Level
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=10, unique=true)
+     */
+    private $nom;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Level
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+}
+
